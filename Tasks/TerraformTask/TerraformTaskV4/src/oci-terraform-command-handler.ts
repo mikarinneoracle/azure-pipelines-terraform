c197582050151f3df0d7a2813aac7a65b6ee7d6c
Mika Rinne <mika.rinne@oracle.com>
2024-04-24 14:46:57 +0200

    Support for OCI added

--- Tasks/TerraformTask/TerraformTaskV4/src/oci-terraform-command-handler.ts
+++ Tasks/TerraformTask/TerraformTaskV4/src/oci-terraform-command-handler.ts
@@ -2,7 +2,6 @@ import tasks = require('azure-pipelines-task-lib/task');
 import {ToolRunner} from 'azure-pipelines-task-lib/toolrunner';
 import {TerraformAuthorizationCommandInitializer} from './terraform-commands';
 import {BaseTerraformCommandHandler} from './base-terraform-command-handler';
-import { readFileSync } from 'fs';
 const fs = require('fs');
 
 export class TerraformCommandHandlerOCI extends BaseTerraformCommandHandler {
@@ -12,9 +11,11 @@ export class TerraformCommandHandlerOCI extends BaseTerraformCommandHandler {
     }
 
     private setupBackend(backendServiceName: string) {
+        tasks.debug("setupBackend -- does nothing ...");
     }
 
     public async handleBackend(terraformToolRunner: ToolRunner) : Promise<void> {
+        tasks.debug("handleBackend -- does nothing ...");
         let backendServiceName = tasks.getInput("backendServiceOCI", true);
         this.setupBackend(backendServiceName);
 
@@ -24,50 +25,34 @@ export class TerraformCommandHandlerOCI extends BaseTerraformCommandHandler {
     }
 
     public async handleProvider(command: TerraformAuthorizationCommandInitializer) : Promise<void> {
-        console.log("key" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "key"));
-        console.log("user" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "user"));
-        console.log("tenancy" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "tenancy"));
-        console.log("fingerprint=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "fingerprint"));
-        console.log("region=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "region"));
+        tasks.debug("key" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "key"));
+        tasks.debug("user" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "user"));
+        tasks.debug("tenancy" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "tenancy"));
+        tasks.debug("fingerprint=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "fingerprint"));
+        tasks.debug("region=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "region"));
 
         if (command.serviceProvidername) {
-            fs.writeFile('./key.pem', tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "key", false),  function(err) {
+            fs.writeFile('/home/opc/.oci/oci_api_key.pem', tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "key", false),  function(err) {
                 if (err) {
                     return console.error(err);
                 }
-                console.log("key.pem created");
+                tasks.debug("/.oci/oci_api_key.pem created");
             });
 
-            /*
-            provider "oci" {
-            tenancy_ocid = "<tenancy-ocid>"
-            user_ocid = "<user-ocid>" 
-            private_key_path = "<rsa-private-key-path>"
-            fingerprint = "<fingerprint>"
-            region = "<region-identifier>"
-            }
-            */
+            var data = "[DEFAULT]\n";
+            data = data + "tenancy=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "tenancy") + "\n";
+            data = data + "user=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "user") + "\n";
+            data = data + "fingerprint=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "fingerprint") + "\n";
+            data = data + "region=" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "region") + "\n";
+            data = data + "key_file=/home/opc/.oci/oci_api_key.pem\n";
+            data = "\n";
 
-            var data = "provider \"oci\" {\n";
-            data = data + "tenancy_ocid = \"" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "tenancy") + "\"\n";
-            data = data + "user_ocid = \"" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "user") + "\"\n";
-            data = data + "fingerprint = \"" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "fingerprint") + "\"\n";
-            data = data + "region = \"" + tasks.getEndpointDataParameterRequired(command.serviceProvidername, "region") + "\"\n";
-            data = data + "private_key_path = \"key.pem\"\n";
-            data = "}\n";
-
-            fs.writeFile('./provider.tf', data,  function(err) {
+            fs.writeFile('/home/opc/.oci/config', data,  function(err) {
                 if (err) {
                     return console.error(err);
                 }
-                console.log("provider.tf created");
-            });
-
-            process.env['TF_VAR_user_ocid']  = tasks.getEndpointDataParameterRequired(command.serviceProvidername, "user");
-            process.env['TF_VAR_tenancy_ocid']  = tasks.getEndpointDataParameterRequired(command.serviceProvidername, "tenancy");        
-            process.env['TF_VAR_fingerprint']  = tasks.getEndpointDataParameterRequired(command.serviceProvidername, "fingerprint");
-            process.env['TF_VAR_private_key_path']  =  "./key.pem";      
-            process.env['TF_VAR_region']  = tasks.getEndpointDataParameterRequired(command.serviceProvidername, "region");  
+                console.log("/.oci/config created");
+            }); 
         }
     }
 }
